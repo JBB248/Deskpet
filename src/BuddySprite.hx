b2af2954eb5bac430f0671d582a8fad138f578aa
JBB248 <joebray64@gmail.com>
2023-11-23 20:06:13 -0600

    Add grabbing the lad

--- src/BuddySprite.hx
+++ src/BuddySprite.hx
@@ -11,9 +11,8 @@ class BuddySprite extends FlxSprite
 {
     public static inline var GRAVITY:Float = 600;
 
-    public var border(default, never):Border = new Border();
+    public var border(default, null):Border = new Border();
 
-    @:allow(Condition)
     var _stateWatcher:FlxFSM<BuddySprite>;
 
     public function new(X:Int = 0, Y:Int = 0)
@@ -26,9 +25,16 @@ class BuddySprite extends FlxSprite
 
         acceleration.y = BuddySprite.GRAVITY;
 
-		_stateWatcher = new FlxFSM(this);
-        _stateWatcher.transitions.add(IdleState, BouncingState, Condition.shouldBounce);
-        _stateWatcher.transitions.add(BouncingState, IdleState, Condition.shouldStopBounce);
+		initStates();
+    }
+
+    function initStates():Void
+    {
+        _stateWatcher = new FlxFSM(this);
+
+        _stateWatcher.transitions.add(IdleState, GrabbedState, Condition.grabbed);
+        _stateWatcher.transitions.add(GrabbedState, IdleState, (sprite) -> !Condition.grabbed(sprite));
+
         _stateWatcher.transitions.start(IdleState);
     }
 
@@ -39,6 +45,9 @@ class BuddySprite extends FlxSprite
         super.update(elapsed);
 
         _stateWatcher.update(elapsed);
+
+        if(!isOnScreen()) // For now, just bring him back if he falls off-screen
+            setPosition();
     }
 
     override public function destroy():Void
@@ -49,49 +58,71 @@ class BuddySprite extends FlxSprite
     }
 }
 
+@:allow(BuddySprite)
 class Condition
 {
-	public static function shouldBounce(sprite:BuddySprite):Bool
+    static function grabbed(sprite:BuddySprite):Bool
     {
-        return sprite._stateWatcher.stateClass == IdleState 
-            && FlxG.mouse.justPressed 
-            && sprite.getHitbox().containsPoint(FlxG.mouse.getScreenPosition());
+        return FlxG.mouse.pressed && FlxG.mouse.overlaps(sprite);
     }
-
-    public static function shouldStopBounce(sprite:BuddySprite):Bool
+    
+	static function shouldBounce(sprite:BuddySprite):Bool
     {
-        return sprite._stateWatcher.stateClass == BouncingState
-            && FlxG.mouse.justPressed 
-            && sprite.getHitbox().containsPoint(FlxG.mouse.getScreenPosition());
+        return FlxG.mouse.justPressed && FlxG.mouse.overlaps(sprite);
     }
 }
 
-interface BuddyState
+class BuddyState extends FlxFSMState<BuddySprite>
 {
-    function onCollide(object1:FlxObject, object2:FlxObject):Void;
-}
-
-class IdleState extends FlxFSMState<BuddySprite> implements BuddyState
-{
-	override public function enter(owner:BuddySprite, fsm:FlxFSM<BuddySprite>):Void 
-    { 
-        trace("Entered idle state");
-    }
-
-	override public function update(elapsed:Float, owner:BuddySprite, fsm:FlxFSM<BuddySprite>):Void { }
-
     public function onCollide(object1:FlxObject, object2:FlxObject):Void { }
 }
 
-class BouncingState extends FlxFSMState<BuddySprite> implements BuddyState
+class IdleState extends BuddyState
+{
+	override public function enter(owner:BuddySprite, fsm:FlxFSM<BuddySprite>):Void 
+    {
+        owner.acceleration.y = BuddySprite.GRAVITY; // Restore gravity
+
+        trace("Entered idle state");
+    }
+}
+
+class GrabbedState extends BuddyState
+{
+    override public function enter(owner:BuddySprite, fsm:FlxFSM<BuddySprite>):Void
+    {
+        owner.velocity.set();
+        owner.maxVelocity.set();
+        owner.acceleration.set();
+        owner.drag.set();
+
+        trace("Entered grabbed state");
+    }
+
+    override public function update(elapsed:Float, owner:BuddySprite, fsm:FlxFSM<BuddySprite>):Void 
+    {
+        owner.x += FlxG.mouse.deltaScreenX;
+        owner.y += FlxG.mouse.deltaScreenY;
+    }
+
+    override public function exit(owner:BuddySprite):Void
+    {
+        owner.velocity.x = FlxG.mouse.deltaScreenX / FlxG.elapsed;
+        owner.velocity.y = FlxG.mouse.deltaScreenY / FlxG.elapsed;
+        owner.maxVelocity.set(1400, 1000);
+
+        owner.drag.x = Math.abs(owner.velocity.x) * 0.3;
+    }
+}
+
+class BouncingState extends BuddyState
 {
 	override public function enter(owner:BuddySprite, fsm:FlxFSM<BuddySprite>):Void
 	{
         owner.y -= 10;
         owner.velocity.x = owner.velocity.x > 0 ? -400 : 400;
         owner.velocity.y = -BuddySprite.GRAVITY * 0.5;
-        owner.maxVelocity.x = 1400;
-        owner.maxVelocity.y = 1000;
+        owner.maxVelocity.set(1400, 1000);
         owner.drag.set();
         owner.elasticity = 2.0;
 
@@ -100,11 +131,11 @@ class BouncingState extends FlxFSMState<BuddySprite> implements BuddyState
 
 	override public function update(elapsed:Float, owner:BuddySprite, fsm:FlxFSM<BuddySprite>):Void 
     { 
-        if(owner.velocity.x > -300 && owner.velocity.x < 300)
+        if(Math.abs(owner.velocity.x) < 300)
 			owner.velocity.x = owner.velocity.x < 0 ? -300 : 300;
     }
 
-    public function onCollide(sprite:FlxObject, wall:FlxObject):Void
+    override public function onCollide(sprite:FlxObject, wall:FlxObject):Void
     { 
         sprite.elasticity = FlxG.random.float(0.5, 3.0);
     }
@@ -113,6 +144,11 @@ class BouncingState extends FlxFSMState<BuddySprite> implements BuddyState
     {
         owner.drag.x = owner.maxVelocity.x * 0.35;
 
-        FlxTween.tween(owner, {elasticity: 0}, 3.0);
+        FlxTween.tween(owner, {elasticity: 0}, 3.0, {
+            onComplete: function(twn:FlxTween) {
+                owner.maxVelocity.set();
+                owner.drag.set();
+            }
+        });
     }
 }
