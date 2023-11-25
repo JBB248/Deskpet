c291880cc952af080887851d741f96418fe65a84
JBB248 <joebray64@gmail.com>
2023-11-24 22:04:34 -0600

    Add shaking recognition

--- src/BuddySprite.hx
+++ src/BuddySprite.hx
@@ -1,5 +1,6 @@
 package;
 
+import flixel.tweens.FlxEase;
 import flixel.FlxG;
 import flixel.FlxObject;
 import flixel.FlxSprite;
@@ -10,6 +11,7 @@ import flixel.util.FlxDestroyUtil;
 class BuddySprite extends FlxSprite
 {
     public static inline var GRAVITY:Float = 600;
+    public static inline var BOUNCINESS:Float = 0.4;
 
     public var border(default, null):Border = new Border();
 
@@ -24,6 +26,7 @@ class BuddySprite extends FlxSprite
 		animation.play("dance");
 
         acceleration.y = BuddySprite.GRAVITY;
+        elasticity = BuddySprite.BOUNCINESS;
 
 		initStates();
     }
@@ -33,7 +36,11 @@ class BuddySprite extends FlxSprite
         _stateWatcher = new FlxFSM(this);
 
         _stateWatcher.transitions.add(IdleState, GrabbedState, Condition.grabbed);
-        _stateWatcher.transitions.add(GrabbedState, IdleState, (sprite) -> !Condition.grabbed(sprite));
+
+        _stateWatcher.transitions.add(GrabbedState, IdleState, (sprite) -> !Condition.shaken && !Condition.grabbed(sprite));
+        _stateWatcher.transitions.add(GrabbedState, BouncingState, (sprite) -> Condition.shaken && !Condition.grabbed(sprite));
+
+        _stateWatcher.transitions.add(BouncingState, GrabbedState, Condition.grabbed);
 
         _stateWatcher.transitions.start(IdleState);
     }
@@ -47,7 +54,7 @@ class BuddySprite extends FlxSprite
         _stateWatcher.update(elapsed);
 
         if(!isOnScreen()) // For now, just bring him back if he falls off-screen
-            setPosition();
+            screenCenter();
     }
 
     override public function destroy():Void
@@ -58,17 +65,13 @@ class BuddySprite extends FlxSprite
     }
 }
 
-@:allow(BuddySprite)
 class Condition
 {
-    static function grabbed(sprite:BuddySprite):Bool
+    public static var shaken:Bool = false;
+
+    public static function grabbed(sprite:BuddySprite):Bool
     {
         return FlxG.mouse.pressed && FlxG.mouse.overlaps(sprite);
-    }
-    
-	static function shouldBounce(sprite:BuddySprite):Bool
-    {
-        return FlxG.mouse.justPressed && FlxG.mouse.overlaps(sprite);
     }
 }
 
@@ -89,6 +92,14 @@ class IdleState extends BuddyState
 
 class GrabbedState extends BuddyState
 {
+    public static inline var OSC:Float = 1.0;
+    public static inline var THRESHOLD:Int = 4;
+    public static inline var COOLDOWN:Int = 3;
+
+    var period:Float;
+    var ticks:Int;
+    var lastVelocity:Float;
+
     override public function enter(owner:BuddySprite, fsm:FlxFSM<BuddySprite>):Void
     {
         owner.velocity.set();
@@ -96,59 +107,103 @@ class GrabbedState extends BuddyState
         owner.acceleration.set();
         owner.drag.set();
 
+        initVars();
+
         trace("Entered grabbed state");
+    }
+
+    function initVars():Void
+    {
+        period = 0;
+        ticks = 0;
+        lastVelocity = 0;
     }
 
     override public function update(elapsed:Float, owner:BuddySprite, fsm:FlxFSM<BuddySprite>):Void 
     {
         owner.x += FlxG.mouse.deltaScreenX;
         owner.y += FlxG.mouse.deltaScreenY;
+
+        if(!Condition.shaken && isShaking())
+            FlxTween.shake(owner, 0.02, 5.0, null, 
+                {onComplete: (twn) -> Condition.shaken = false});
+    }
+
+    /**
+     * This only checks for _vertical_ shaking (up and down)
+     */
+    function isShaking():Bool
+    {
+        if(Condition.shaken) return true;
+
+        period += FlxG.elapsed;
+        var velocity = FlxG.mouse.deltaScreenY / FlxG.elapsed;
+        if(velocity * lastVelocity < 0)
+        {
+            if(period <= GrabbedState.OSC) ticks++;
+               period = 0;
+
+            if(ticks >= GrabbedState.THRESHOLD)
+                return Condition.shaken = true;
+        }
+        else if(period >= GrabbedState.COOLDOWN)
+            ticks = 0;
+
+        lastVelocity = velocity;
+        return false;
     }
 
     override public function exit(owner:BuddySprite):Void
     {
-        owner.velocity.x = FlxG.mouse.deltaScreenX / FlxG.elapsed;
-        owner.velocity.y = FlxG.mouse.deltaScreenY / FlxG.elapsed;
-        owner.maxVelocity.set(1400, 1000);
+        if(!Condition.shaken)
+        {
+            owner.velocity.x = FlxG.mouse.deltaScreenX / FlxG.elapsed;
+            owner.velocity.y = FlxG.mouse.deltaScreenY / FlxG.elapsed;
+            owner.maxVelocity.set(1400, 1000);
+            owner.drag.x = Math.abs(owner.velocity.x) * 0.3;
+        }
 
-        owner.drag.x = Math.abs(owner.velocity.x) * 0.3;
+        owner.offset.set();
+        Condition.shaken = false;
     }
 }
 
 class BouncingState extends BuddyState
 {
+    public static inline var MINSPEEDX:Int = 300;
+
 	override public function enter(owner:BuddySprite, fsm:FlxFSM<BuddySprite>):Void
 	{
-        owner.y -= 10;
-        owner.velocity.x = owner.velocity.x > 0 ? -400 : 400;
-        owner.velocity.y = -BuddySprite.GRAVITY * 0.5;
-        owner.maxVelocity.set(1400, 1000);
-        owner.drag.set();
+        owner.velocity.set(8000, 8000).copyTo(owner.maxVelocity);
+        if(FlxG.mouse.deltaScreenX / FlxG.elapsed < 0)
+            owner.velocity.x *= -1;
+        if(FlxG.mouse.deltaScreenY / FlxG.elapsed < 0)
+            owner.velocity.y *= -1;
+
+        owner.acceleration.y = 0;
         owner.elasticity = 2.0;
+
+        FlxTween.tween(owner.maxVelocity, {x: FlxG.width, y: FlxG.height}, 5.0, 
+            {onComplete: (twn) -> owner.acceleration.y = BuddySprite.GRAVITY});
 
         trace("Entered bouncing state");
 	}
 
 	override public function update(elapsed:Float, owner:BuddySprite, fsm:FlxFSM<BuddySprite>):Void 
     { 
-        if(Math.abs(owner.velocity.x) < 300)
-			owner.velocity.x = owner.velocity.x < 0 ? -300 : 300;
+        if(Math.abs(owner.velocity.x) < BouncingState.MINSPEEDX)
+			owner.velocity.x = owner.velocity.x < 0 ? -BouncingState.MINSPEEDX : BouncingState.MINSPEEDX;
     }
 
     override public function onCollide(sprite:FlxObject, wall:FlxObject):Void
-    { 
+    {
         sprite.elasticity = FlxG.random.float(0.5, 3.0);
     }
 
     override public function exit(owner:BuddySprite) 
     {
-        owner.drag.x = owner.maxVelocity.x * 0.35;
-
-        FlxTween.tween(owner, {elasticity: 0}, 3.0, {
-            onComplete: function(twn:FlxTween) {
-                owner.maxVelocity.set();
-                owner.drag.set();
-            }
-        });
+        owner.velocity.set();
+        owner.maxVelocity.set();
+        owner.elasticity = BuddySprite.BOUNCINESS;
     }
 }
